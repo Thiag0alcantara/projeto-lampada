17b6990c4783d467f98c6a9c85670c75c60b6f37
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2024-01-04 18:11:40 -0300

    Initial commit

---
+++ script.lampada.js
@@ -0,0 +1,31 @@
+let lampada = window.document.getElementById('lamp')
+let lampon = window.document.getElementById('lig')
+let lampoff = window.document.getElementById('des')
+
+function verfLamp(){
+    return lamp.src.indexOf ( 'quebrada' ) > -1
+}
+
+function turnon(){
+    if( !verfLamp() ){
+
+        lamp.src = 'ligada.jpg'
+    }
+}
+
+
+function turnoff(){
+    if( !verfLamp() ){
+        
+        lamp.src = `desligada.jpg` 
+    }
+}
+
+function lampbroken(){
+    lamp.src = 'quebrada.jpg'
+}
+
+lampada.addEventListener('mouseover',turnon);
+lampada.addEventListener('mouseleave',turnoff);
+lampada.addEventListener('dblclick',lampbroken);
+
